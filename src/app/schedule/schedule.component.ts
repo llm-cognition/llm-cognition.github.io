93a8e6009ab6462183bf6ea41ebd36cc5edfefe5
Badr AlKhamissi <badr@khamissi.com>
2024-07-12 08:21:58 -0700

    updated accepted papers presentation

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -11,7 +11,7 @@ export class ScheduleComponent implements OnInit {
     {time: '09:00', title: 'Introduction and Opening remarks', speaker: '-'},
     {time: '09:10', title: 'Invited Talk 1', speaker: 'Subbarao Kambhampati'},
     {time: '09:40', title: 'Invited Talk 2', speaker: 'Antoine Bosselut'},
-    {time: '10:10', title: 'Poster Session 1', speaker: '-'},
+    {time: '10:10', title: 'Poster Session 1', speaker: 'Papers #1-#32 (see below)'},
     {time: '11:10', title: 'Break', speaker: '-'},
     {time: '11:30', title: 'Invited Talk 3', speaker: 'Chelsea Finn'},
     {time: '12:00', title: 'Invited Talk 4', speaker: 'Ethan Wilcox'},
@@ -19,7 +19,7 @@ export class ScheduleComponent implements OnInit {
     {time: '14:00', title: 'Panel Discussion', speaker: '-'},
     {time: '15:00', title: 'Contributed Talk 1', speaker: 'TBD'},
     {time: '15:15', title: 'Contributed Talk 2', speaker: 'TBD'},
-    {time: '15:30', title: 'Poster Session 2', speaker: '-'},
+    {time: '15:30', title: 'Poster Session 2', speaker: 'Papers #33-#63 (see below)'},
     {time: '16:30', title: 'Invited Talk 5', speaker: 'Melanie Mitchell'},
     {time: '17:00', title: 'End', speaker: '-'}
   ];
